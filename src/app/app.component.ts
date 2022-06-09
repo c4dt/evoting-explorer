a1231d496eb3476da785d6156ce6d414512bdde1
Linus Gasser <linus.gasser@epfl.ch>
2022-06-09 16:59:57 +0200

    Adding mixes and partial decryption

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import {Component, OnInit} from '@angular/core';
-import {SkipchainRPC} from '@dedis/cothority/skipchain';
+import {SkipBlock, SkipchainRPC} from '@dedis/cothority/skipchain';
 import {Roster} from "@dedis/cothority/network";
-import {Ballot, Election, Transaction} from "@dedis/cothority/evoting/proto";
+import {Election, Transaction} from "@dedis/cothority/evoting/proto";
 
 interface BallotBlock {
     user: number,
     block: number,
+}
+
+interface FinalizeBlock {
+    block: number,
+    node: string,
+    votes: number,
 }
 
 @Component({
@@ -29,6 +35,8 @@ export class AppComponent implements OnInit {
     public election: Election | undefined;
     public elections: Election[] = [];
     public ballots: BallotBlock[] = [];
+    public partials: FinalizeBlock[] = [];
+    public mixes: FinalizeBlock[] = [];
 
     constructor() {
     }
@@ -39,7 +47,7 @@ export class AppComponent implements OnInit {
         for (const link of links) {
             const block = await this.sc.getSkipBlockByIndex(link, 1);
             const tx = Transaction.decode(block.skipblock.data);
-            if (tx.election !== null){
+            if (tx.election !== null) {
                 console.log("Election: ", tx.election.name["en"], tx.election.subtitle["en"]);
                 console.dir(tx.election);
                 elections.unshift(tx.election);
@@ -67,20 +75,43 @@ export class AppComponent implements OnInit {
     async showElection(e: Election) {
         this.election = e;
         this.ballots = [];
+        this.partials = [];
+        this.mixes = [];
         const blocks = await this.sc.getUpdateChain(e.id, false, false, 1);
-        for (const block of blocks.reverse()){
-            if (block.data.length > 0){
+        for (const block of blocks.reverse()) {
+            if (block.data.length > 0) {
                 const tx = Transaction.decode(block.data);
-                if (tx.ballot !== null){
+                if (tx.ballot !== null) {
                     console.log("Ballot of block", block.index);
                     console.dir(tx.ballot);
                     this.ballots.push({user: tx.ballot.user, block: block.index});
+                } else if (tx.mix !== null) {
+                    console.log("Mix in block", block.index);
+                    console.dir(tx.mix);
+                    this.mixes.push(this.createFinalizedBlock(block, tx.mix.nodeid, tx.mix.ballots.length));
+                } else if (tx.partial !== null) {
+                    console.log("Partial decryption in block", block.index);
+                    console.dir(tx.partial);
+                    this.partials.push(this.createFinalizedBlock(block, tx.partial.nodeid, tx.partial.points.length));
                 }
             }
         }
     }
 
-    async showOverview(){
+    createFinalizedBlock(block: SkipBlock, nodeID: Buffer, votes: number): FinalizeBlock {
+        const nodeSI = block.roster.list.find((si) => si.id.equals(nodeID));
+        let node = "Not found";
+        if (nodeSI !== undefined) {
+            node = nodeSI.description;
+        }
+        return {
+            block: block.index,
+            node,
+            votes,
+        }
+    }
+
+    async showOverview() {
         this.election = undefined;
     }
 }
